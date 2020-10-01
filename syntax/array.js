8710bf4d293c6cba5c4def1f7072e6ac7747bfbb
gnsia <gnsia@naver.com>
2020-10-01 19:30:51 +0900

    nodejs excercise

---
+++ syntax/array.js
@@ -0,0 +1,13 @@
+var arr = ['A', 'B', 'C', 12, true];
+
+console.log(arr[0]);
+console.log(arr[1]);
+
+arr[2] = 3;
+console.log(arr[2]);
+console.log(arr.length);
+
+console.log(arr[3]);
+
+arr.push('E');
+console.log(arr);
---
+++ syntax/function2.js
@@ -0,0 +1,11 @@
+
+//console.log(Math.round(1.6));
+//console.log(Math.round(1.4));
+
+function sum(first, second){ // parameter
+  console.log('a');
+  return first+second; // return을 만나면 즉시 수행정지
+  console.log('b'); // 출력되지 않는다.
+}
+
+console.log(sum(2,4)); // argument
