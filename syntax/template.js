117c1eb8d0fdd43fc4aaf1955adcf30958521d9d
gnsia <gnsia@naver.com>
2020-10-01 15:35:01 +0900

    javascript syntax excercise

---
+++ syntax/template.js
@@ -0,0 +1,13 @@
+var name = 'gnsia';
+var letter = 'Dear '+name+' Lorem ipsum dolor sit amet,\n\
+consectetur adipisicing elit, \n\
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\
+Ut enim ad minim veniam, \n\
+quis nostrud exercitation '+name+' ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '+name+' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ${name} ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in ${100*100} voluptate velit esse cillum dolore eu fugiat nulla pariatur. ${name} Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+console.log(letter);
+
+var a = 1;
